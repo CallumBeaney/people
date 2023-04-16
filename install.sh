1585d8fedd28a075b6651cd28de25dc5e3e7ddf1
chilldude <matinhuseynzade@gmail.com>
2023-04-17 00:58:23 +0300

    Updated readme about the usage of the installer.

--- install.sh
+++ install.sh
@@ -4,12 +4,17 @@ gcc -o people src/main.c src/helpers.c
 
 # Get the directory of the currently running script
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-echo $SCRIPT_DIR
+
 # Check if .bashrc exists
 if [ -e "$HOME/.bashrc" ]; then
-    # Append lines to .bashrc
-    echo 'export PATH="'$SCRIPT_DIR'/people:$PATH"' >> "$HOME/.bashrc"
-    echo 'alias people="'$SCRIPT_DIR'/people"' >> "$HOME/.bashrc"
-    source ~/.bashrc
+        # Append lines to .bashrc if people command does not exist
+
+        if ! command -v people >/dev/null 2>&1; then
+                echo 'export PATH="'$SCRIPT_DIR'/people:$PATH"' >> "$HOME/.bashrc"
+                echo 'alias people="'$SCRIPT_DIR'/people"' >> "$HOME/.bashrc"
+        fi
+
+        source ~/.bashrc
+        echo People is successfully installed! Now, open a new terminal window to be able to run the program.
 fi
 
