b50bd51617877056c578f27e5b74cfdd2139cf32
Callum Beaney <79003912+CallumBeaney@users.noreply.github.com>
2023-10-23 13:52:39 +0100

    clearup 2

--- src/main.c
+++ src/main.c
@@ -8,33 +8,36 @@
 
 int main(int argc, char *argv[])
 {
-    int result;
-
     if (argc == 1) {   
-        return result = checkAllPersons(); /// if they are just running ./people
+        return checkAllPersons(); /// if they are just running ./people
     }
     else if (argc == 3 && strcmp(argv[1], "forget") == 0 && strcmp(argv[2], "all") == 0) {
-        return result = forgetAll(); /// people forget all
+        return forgetAll(); /// people forget all
     }
     else if (argc >= 3 && strcmp(argv[1], "forget") == 0) {
-        return result = forgetPerson(argc, argv); /// people forget name name 
+        return forgetPerson(argc, argv); /// people forget name name 
     } 
     else if (argc == 3 && strcmp(argv[1], "days") == 0) {
-       return result = setNumberOfDaysBenchmark(argv); /// people days [number] 
+       return setNumberOfDaysBenchmark(argv); /// people days [number] 
     }
     else if ((argc >= 3) && strcmp(argv[1], "add") == 0) {
-        return result = addPerson(argc, argv); /// people add name name
+        return addPerson(argc, argv); /// people add name name
     }
     else if (argc == 3 && strcmp(argv[1], "check") == 0 && strcmp(argv[2], "all") == 0) { 
-        return result = checkAllPersons();  /// people check [all]  
+        return checkAllPersons();  /// people check [all]  
     }
     else if (strcmp(argv[1], "check") == 0 && argc >= 3) { 
-        return result = checkSpecificPerson(argc, argv); /// people check [name OR substring thereof] 
+        return checkSpecificPerson(argc, argv); /// people check [name OR substring thereof] 
     } 
     else if (argc == 2 && strcmp(argv[1], "info") == 0) {
-        return result = errorMessage(0); // User doesn't know how to use the program and wants to know
+        return errorMessage(0); // User doesn't know how to use the program and wants to know
     } 
     else {
-        return result = errorMessage(1); // User doesn't know how to use the program and needs help
+        return errorMessage(1); // User doesn't know how to use the program and needs help
     }
+
+    /// the program should never get here
+    char* url = "https://github.com/CallumBeaney/people";
+    printf("ERROR: Your input command has fallen through the parsing tree.\nIf you have the time, please kindly report an issue to the link below: \033]8;%s\007%s\033]8;;\n\033[0m \n\n", url, url); 
+    return errorMessage(1);
 }
